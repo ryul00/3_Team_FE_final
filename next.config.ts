6879ff40384c4b090fee38a17e49e0e0312bb43d
nayeong lee <lny021102@gmail.com>
2024-11-10 22:14:48 +0900

    chore(all): 전역 스타일 설정 및 필수 화면 폴더 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  compiler: {
+    // styled-components 지원 활성화
+    // styledComponents: true,
+  },
+  devIndicators: {
+    buildActivity: false, // 빌드 배지 비활성화
+    appIsrStatus: false,  // ISR 관련 배지 비활성화
+  },
 };
 
 export default nextConfig;
---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,9 @@
+// 로딩 시 첫화면
+
+export default function Home() {
+	return (
+		<div>
+			여기는 홈페이지
+		</div>
+	);
+}
