e09fdf4cd3dd3aba5f9d8d3dde82313e6b216b7b
황유빈 <bnm0218@naver.com>
2024-11-18 17:23:55 +0900

    Feat(api):엑세스토큰을 담은 api 요청 코트 (작업 중..)

---
+++ src/api/fetchData.ts
@@ -0,0 +1,31 @@
+"use server";
+import { cookies } from "next/headers";
+
+export async function fetchData(endPoint: string, method: "GET" | "POST" | "DELETE" | "PATCH" | "PUT", body?: object) {
+    const BASE_URL = "https://byulchaek-api.shop";
+    const url = `${BASE_URL}${endPoint}`;
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get("accessToken")
+    console.log("데이터 패치의 쿠키 저장소", cookieStore);
+    console.log("데이터 패치의 엑세스 토큰", accessToken);
+
+    const headers: HeadersInit = {
+        "Content-Type": "application/json",
+    };
+   
+        headers["Authorization"] = `Bearer ${accessToken}`;
+        console.log('헤더',headers)
+
+    const response = await fetch(url, {
+        method: method,
+        headers: headers,
+        body: body ? JSON.stringify(body) : undefined,
+        credentials: "include",
+    });
+
+    if (!response.ok) {
+        console.error("요청을 다시 확인 해주세여", response.statusText);
+        throw new Error(`HTTP ERROR, STATUS:${response.status}`);
+    }
+    return response.json();
+}
