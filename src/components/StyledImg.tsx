2f475538d3d7b86002204f8cf6b8b5a925905477
황유빈 <bnm0218@naver.com>
2024-11-18 17:20:44 +0900

    Fix(StyledImg):커스텀 이미지 설정 수정

--- src/components/StyledImg.tsx
+++ src/components/StyledImg.tsx
@@ -3,39 +3,42 @@
 // 이미지를 삽입해야 할 때 이 컴포넌트를 활용
 
 interface StyledImgProps {
-	$width?: string;
-	$height?: string;
-	$margin?: string;
-	$padding?: string;
-	$borderradius?: string;
-	$border?: string;
-	$cursor?: string;
+    $width?: string;
+    $height?: string;
+    $margin?: string;
+    $padding?: string;
+    $borderradius?: string;
+    $border?: string;
+    $cursor?: string;
+    $alignSelf?: string;
 }
 
 const StyledImg: React.FC<React.ImgHTMLAttributes<HTMLImageElement> & StyledImgProps> = ({
-	$width = "auto",
-	$height = "auto",
-	$margin = "0",
-	$padding = "0",
-	$borderradius = "0",
-	$border = "none",
-	$cursor = "auto",
-	...props
+    $width = "auto",
+    $height = "auto",
+    $margin = "0",
+    $padding = "0",
+    $borderradius = "0",
+    $border = "none",
+    $cursor = "auto",
+    $alignSelf = "auto",
+    ...props
 }) => {
-	return (
-		<img
-			style={{
-				width: $width,
-				height: $height,
-				margin: $margin,
-				padding: $padding,
-				borderRadius: $borderradius,
-				border: $border,
-				cursor: $cursor,
-			}}
-			{...props}
-		/>
-	);
+    return (
+        <img
+            style={{
+                width: $width,
+                height: $height,
+                margin: $margin,
+                padding: $padding,
+                borderRadius: $borderradius,
+                border: $border,
+                cursor: $cursor,
+                alignSelf: $alignSelf,
+            }}
+            {...props}
+        />
+    );
 };
 
 export default StyledImg;
