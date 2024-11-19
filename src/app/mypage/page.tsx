756315fad623420f24930d658e651f1ab430217c
nayeong lee <lny021102@gmail.com>
2024-11-20 03:40:49 +0900

    feat(mypage): Header 컴포넌트 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,13 +1,14 @@
 import CustomColumn from "@/components/CustomColumn";
 import MyReadingRecord from "./MyReadingRecord";
 import UserInfoBox from "./UserInfoBox";
-
+import MyPageHeader from "./components/MyPageHeader";
 
 export default function page() {
 
     return (
         <div className="bg-[#473322] w-full h-full min-h-screen pt-4">
             <CustomColumn $gap="4rem">
+                <MyPageHeader />
                 <UserInfoBox />
                 <MyReadingRecord />
             </CustomColumn>
