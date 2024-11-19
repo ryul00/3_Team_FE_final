636f2453a08ec22e307e1b0d4569d98411b22b54
황유빈 <bnm0218@naver.com>
2024-11-20 03:43:41 +0900

    Fix(mypage):라우터 경로 수정

--- src/app/mypage/components/MyPageHeader.tsx
+++ src/app/mypage/components/MyPageHeader.tsx
@@ -7,13 +7,13 @@ import { FaHome } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 
 export default function MyPageHeader() {
-	const router = useRouter();
+    const router = useRouter();
 
-	return (
-		<CustomRow $width="90%" $alignitems="center" $justifycontent="flex-end">
-			<CustomButton $backgroundColor="transparent" $width="auto" $padding="0" onClick={() => router.push("/mypage")}>
-				<FaHome style={{ color: "#2C1F13", fontSize: "2rem" }} />
-			</CustomButton>
-		</CustomRow>
-	);
+    return (
+        <CustomRow $width="90%" $alignitems="center" $justifycontent="flex-end">
+            <CustomButton $backgroundColor="transparent" $width="auto" $padding="0" onClick={() => router.push("/homepage")}>
+                <FaHome style={{ color: "#2C1F13", fontSize: "2rem" }} />
+            </CustomButton>
+        </CustomRow>
+    );
 }
