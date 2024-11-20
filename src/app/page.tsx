b368a1b86bdf38c0d10c1b11f827923e60df4030
nayeong lee <lny021102@gmail.com>
2024-11-20 16:35:34 +0900

    fix(app/page.tsx): 로그인화면이 첫 화면이 되도록 리다이렉트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // '/' 경로에 접근 시 '/homepage'로 리다이렉트
-  redirect('/homepage');
+  // '/' 경로에 접근 시 '/loginpage'로 리다이렉트
+  redirect('/loginpage');
 }
