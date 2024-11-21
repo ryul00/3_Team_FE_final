9dedcce25404bdd6e8051428cea72c5a9cf46101
황유빈 <bnm0218@naver.com>
2024-11-21 23:01:14 +0900

    Fix(components):Input 설정 수정

--- src/components/modal/ModalTextArea.tsx
+++ src/components/modal/ModalTextArea.tsx
@@ -1,37 +1,29 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 
 interface ModalTextAreaProps {
-	placeholder?: string;
-	maxLength: number;
+    placeholder?: string;
+    maxLength: number;
+    textArea?: string;
+    setTextArea: any;
 }
 
-const ModalTextArea: React.FC<ModalTextAreaProps> = ({
-	placeholder = "",
-	maxLength,
-}) => {
-	const [value, setValue] = useState("");
+const ModalTextArea: React.FC<ModalTextAreaProps> = ({ placeholder = "", maxLength, textArea, setTextArea }) => {
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (e.target.value.length <= maxLength) {
+            setTextArea(e.target.value);
+        }
+    };
 
-	const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-		if (e.target.value.length <= maxLength) {
-			setValue(e.target.value);
-		}
-	};
-
-	return (
-		<div className="relative w-full">
-			<textarea
-				value={value}
-				onChange={handleChange}
-				placeholder={placeholder}
-				className="w-full h-[10rem] px-4 py-2 border rounded-lg outline-none border-[#7A6B52] resize-none"
-			/>
-			<p className="absolute bottom-[-20px] right-2 text-sm text-[#7A6B52]">
-				{value.length}/{maxLength}
-			</p>
-		</div>
-	);
+    return (
+        <div className="relative w-full">
+            <textarea value={textArea || ""} onChange={handleChange} placeholder={placeholder} className="w-full h-[10rem] px-4 py-2 border rounded-lg outline-none border-[#7A6B52] resize-none" />
+            <p className="absolute bottom-[-20px] right-2 text-sm text-[#7A6B52]">
+                {textArea?.length}/{maxLength}
+            </p>
+        </div>
+    );
 };
 
 export default ModalTextArea;
