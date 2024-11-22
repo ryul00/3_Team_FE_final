f943b158d95b02aa24389f3e09bfa5b01b18997c
nayeong lee <lny021102@gmail.com>
2024-11-22 21:52:37 +0900

    next.xonfig.ts에 cors 허용 추가 - kakaomap 관련

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,28 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+
+  async headers() {
+    return [
+      {
+        source: '/api/:path*', // API 라우트에 대해 CORS 헤더를 추가
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // 모든 도메인 허용 (필요에 따라 변경)
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS', // 허용할 HTTP 메서드
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization', // 필요한 헤더 명시
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
